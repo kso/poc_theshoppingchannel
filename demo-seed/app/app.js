0c173c3d48ef4d87e44f7abc3b321193e204e0e8
Peter Schretlen <peter.schretlen@groupbyinc.com>
2016-01-19 19:57:07 +0000

    remove hashbang from URL, use html5 mode

--- demo-seed/app/app.js
+++ demo-seed/app/app.js
@@ -7,10 +7,13 @@ angular.module("groupByDemo", [
 	'groupByDemo.product',
 	'groupByDemo.typeahead',
 	]).
-config(['$routeProvider', function($routeProvider){
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 	$routeProvider.
 		when("/", {templateUrl: "search/search-results.html", controller: "searchCtrl"}).
 		when("/q/:query", {templateUrl: "search/search-results.html", controller: "searchCtrl"}).
 		when("/product/:id", {templateUrl: "product/product.html", controller: "productCtrl"}).
 		otherwise({redirectTo: "/"});
+
+	// use the HTML5 History API
+    $locationProvider.html5Mode(true);
 }]);
