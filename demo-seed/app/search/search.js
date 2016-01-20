8eb0fbefbc031b94d8207b34401013db06bda951
Peter Schretlen <peter.schretlen@groupbyinc.com>
2016-01-20 16:24:29 +0000

    adding back scope on search controller, needed for current page

--- demo-seed/app/search/search.js
+++ demo-seed/app/search/search.js
@@ -1,32 +1,31 @@
 'use strict';
 
 angular.module("groupByDemo.search",['ui.bootstrap'])
-	.controller('searchCtrl', ['apiService', '$routeParams', function (apiService, $routeParams) {
+	.controller('searchCtrl', ['$scope', 'apiService', '$routeParams', function ($scope, apiService, $routeParams) {
 
+		$scope.currentPage = 1;
+		
 		var view_model = this;
 
 		view_model.query = $routeParams.query;
-		view_model.currentPage = 1;
 		view_model.pageSize = 30;
 		view_model.resultSummary =  "";
 
 		view_model.search = function () {
 
-			console.log("running search")
-
 			var parameters = {
-				skip : view_model.pageSize * (view_model.currentPage - 1),
+				skip : view_model.pageSize * ($scope.currentPage - 1),
 				pageSize : view_model.pageSize,
 				query : view_model.query
 			};
 
 		  	apiService.search(parameters).success(function(data){
-				console.log(data);
+				//console.log(data);
 				view_model.totalRecordCount = data.totalRecordCount;
 				view_model.resultList = data.records;
 				view_model.navigationList = data.availableNavigation;
 
-				var firstResult = view_model.pageSize * (view_model.currentPage - 1) + 1
+				var firstResult = view_model.pageSize * ($scope.currentPage - 1) + 1
 				var lastResult =  firstResult + view_model.pageSize - 1
 				view_model.resultSummary =  firstResult.toString() + " - " + lastResult.toString() + " of " +  view_model.totalRecordCount.toString() + " Products";
 			});
