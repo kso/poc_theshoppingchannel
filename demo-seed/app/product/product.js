d0a5955b05a9b63b6a235a671ce76dc2ba30feb8
Peter Schretlen <peter.schretlen@groupbyinc.com>
2016-02-09 10:51:41 +0000

    use ui-router on product page

--- demo-seed/app/product/product.js
+++ demo-seed/app/product/product.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('groupByDemo.product', ['ui.bootstrap'])
-	.controller('productCtrl', ['apiService', '$routeParams', function(apiService, $routeParams){
+	.controller('productCtrl', ['apiService', '$stateParams', function(apiService, $stateParams){
 		
 		var view_model = this;
 
-		apiService.getProduct($routeParams.id).then(function(record){
+		apiService.getProduct($stateParams.id).then(function(record){
 			view_model.meta = record.allMeta;
+
+			//if color is not an array, convert to array
+			if(!Array.isArray(record.allMeta.Qcolor)){
+				record.allMeta.Qcolor = [record.allMeta.Qcolor];
+			}
 		});
 
 	}]);
